02185d76a154c9ae1142ebf9a2c5ccbd08bb3477
unknown <wildcatrobotics2011@gmail.com>
2018-08-24 00:15:21 -0700

    db stuff good

--- index.js
+++ index.js
@@ -1,7 +1,4 @@
 var EXA = require('./lib/expressServer')
     AUH = require('./lib/authHandler')
-    DBS = require('./lib/dbService');
-
-
-EXA.dbinsert = DBS.insert;
-EXA.dbdrop = DBS.drop;
+    DBS = require('./lib/dbService'),
+    DTS = require('./lib/dataStruct')
